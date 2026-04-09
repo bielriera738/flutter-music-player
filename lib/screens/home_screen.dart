b7cf42c86e5b695920c2fe1a987c5dca8366bef9
Tu Nombre <tu@email.com>
2026-04-09 17:57:59 +0200

    Fix: Remove unused imports and add .env to assets

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:just_audio/just_audio.dart';
 import '../providers/music_provider.dart';
 import '../widgets/song_list_widget.dart';
 import '../widgets/player_widget.dart';
--- lib/widgets/player_widget.dart
+++ lib/widgets/player_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:just_audio/just_audio.dart';
 import '../providers/music_provider.dart';
 
 class PlayerWidget extends StatefulWidget {
